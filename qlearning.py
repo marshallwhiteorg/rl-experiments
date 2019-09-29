8692a4ed456a591cfb218be3e26e380cde04a422
marshall <marshall@marshallwhite.org>
2019-09-28 20:07:28 -0400

    wip

--- qlearning.py
+++ qlearning.py
@@ -11,8 +11,11 @@ import torch
 
 
 #ENV_NAME = 'MiniGrid-DoorKey-5x5-v0'
-ENV_NAME = 'MiniGrid-Empty-5x5-v0'
-env = gym_minigrid.wrappers.FlatObsWrapper(gym.make(ENV_NAME))
+#ENV_NAME = 'MiniGrid-Empty-5x5-v0'
+#ENV_NAME = 'CartPole-v0'
+ENV_NAME = 'Taxi-v2'
+env = gym.make(ENV_NAME)
+#env = gym_minigrid.wrappers.FlatObsWrapper(gym.make(ENV_NAME))
 logging.info(ENV_NAME)
 # A function that will format the observation space for easier use
 #_, preprocess_obss = get_obss_preprocessor(env.observation_space)
@@ -29,7 +32,6 @@ def make_policy(q, num_actions, epsilon):
 
     """
     def policy(observation):
-        print(q[observation])
         best_action_idx = np.argmax(q[observation])
         distribution = []
         for action_idx in range(num_actions):
@@ -56,13 +58,14 @@ def q_learning(env, num_episodes, alpha, gamma, epsilon):
     statistics = plotting.EpisodeStats(
         episode_lengths=np.zeros(num_episodes),
         episode_rewards=np.zeros(num_episodes))
-    q = defaultdict(lambda: np.zeros(env.action_space.n))
-    print(q)
+    q = np.zeros([env.observation_space.n, env.action_space.n])
     for episode_idx in range(num_episodes):
+        if (episode_idx + 1) % 10 == 0:
+            print("\nEpisode {}/{}"
+                  .format(episode_idx + 1, num_episodes))
         observation = torch.tensor(env.reset())
         terminal = False
         t = 0
-        episode = []
         while not terminal:
             policy = make_policy(q, env.action_space.n, epsilon)
             action_distribution = policy(observation)
@@ -70,24 +73,22 @@ def q_learning(env, num_episodes, alpha, gamma, epsilon):
                                       p=action_distribution)
             next_observation, reward, done, _ = env.step(action)
             next_observation = torch.tensor(next_observation)
-            episode.append([observation, action, reward, next_observation, done])
             statistics.episode_rewards[episode_idx] += reward
             statistics.episode_lengths[episode_idx] = t
-            print("\nStep {} @ Episode {}/{} ({})"
-                  .format(t, episode_idx + 1, num_episodes,
-                          statistics.episode_rewards[episode_idx - 1]), end="")
             best_next_action_value = max(
                 [q[next_observation][next_action] - q[observation][action]
                  for next_action
                  in np.arange(len(q[next_observation]))])
             q[observation] += alpha * (reward + gamma * best_next_action_value)
-            observation = next_observation
             if done:
                 terminal = True
+            else:
+                observation = next_observation
+                t += 1
     return q, statistics
 
 
 
 if __name__ == '__main__':
-    q, stats = q_learning(env, 500, 1.0, .5, .1)
+    q, stats = q_learning(env, 400, .1, 1, .1)
     plotting.plot_episode_stats(stats)
